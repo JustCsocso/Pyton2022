c97fbb8c7c0b4d49ad63325a7c48ef99cd870754
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-10-27 13:05:25 +0200

    d

---
+++ mproject.py
@@ -0,0 +1,9 @@
+print("Sebesség kiszámítás (v)")
+a=int(input(" (s) Kérem a megtett utat: "))
+b=int(input(" (t) Kérem az eltelt időt: "))
+
+sebesség=a/b
+print("Képlet:")
+print("\t sebesség - út/ idő")
+
+print("Eredmény", sebesség) 
