44060da13b594a0b8a5d0c00bdc26eb11c6659cc
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-11-17 13:19:04 +0100

    as

--- masodfok.py
+++ masodfok.py
@@ -1,24 +1,66 @@
-import math 
+import math
 
-#a*X2+b*x+c
+def egyenlet(a,b,c):
+    szoveg="0="
+    if a!=0:
+        szoveg+=str(a)+"x²"
+    if b!=0:
+        szoveg+=" + "+str(b)+"x"
+    elif b<0:
+        szoveg+=" - "+str(b)+"x"
+
+    if c!=0:
+        szoveg+=" + "+str(c)
+    elif c<0:
+        szoveg+=" - "+str(c)       
+    
+
+    return szoveg
+    
+
+
+
+
+
+
+
+#aX2+bX+c
 
 a = int(input("a="))
 b = int(input("b="))
 c = int(input("c="))
-
-#(-b+-gyök(b2-4ac)) / 2a
+#(-b+gyök(b2-4*ac)) /2a
 
 #print(math.sqrt(3))
+
+x1=""
+x2=""
 diszkriminans=b*b-4*a*c
 if diszkriminans<0:
     print("nincs megoldás")
 elif diszkriminans==0:
     megoldas=-b /(2*a)
+    x1=megoldas
+    x2=megoldas
     print("1 megoldás: {}".format(megoldas))
 else:
     x1=(-b+math.sqrt(diszkriminans)) / (2*a)
-    x2=(-b-math.sqrt(diszkriminans)) / (2*a)
+    x1=(-b-math.sqrt(diszkriminans)) / (2*a)
+    print("2 megoldás: x1:{}, x2:{}".format(x1,x2))
 
-#gyok=math.sqrt(1)
+     
+#gyök=math.sqrt(1)
 #print(gyok)
+print(egyenlet(a,b,c))
+#a*(x-x1)*(x-x2)=0
+print(a)
+print(x1)
+print(x2)
+
+
+
+
+
+
+
 
