cd46b3de8341ebfd2047c1b143e6ffee999a427e
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 13:20:31 +0100

    sds

---
+++ szamBe.py
@@ -0,0 +1,16 @@
+#Szambekérő modul
+#Többféle paraméterezéssel
+#2022.11.18. Pintér Csongor
+
+def szamBe(szoveg,tort=False,minimun=False,maximum=False):
+    print(szoveg)
+    print(tort)
+
+
+szamBe("Aggggyá meg számot: ", tort=True)
+
+print("fsfsfs", end="")
+print("1221212")
+
+
+
