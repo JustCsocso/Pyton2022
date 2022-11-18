549446b940280a08358282e8996d8a2db1927ae9
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 14:04:14 +0100

    ss

--- szamBe.py
+++ szamBe.py
@@ -2,15 +2,27 @@
 #Többféle paraméterezéssel
 #2022.11.18. Pintér Csongor
 
-def szamBe(szoveg,tort=False,minimun=False,maximum=False):
-    print(szoveg)
-    print(tort)
+def szamBe(szoveg,tort=False,minimum=False,maximum=False):
+#    print(szoveg)
+#    print(tort)
+#    print(minimum)
+    hiba=True
+    while hiba:
+        try:
+            if tort:
+                szam=float(input(szoveg))
+            else:
+                szam=int(input(szoveg))
 
+        except:
+            print("Hiba!")
+        else:
+            hiba=False
 
-szamBe("Aggggyá meg számot: ", tort=True)
+szamBe("Aggyá meg számot: ",minimum=10,tort=True)
 
-print("fsfsfs", end="")
-print("1221212")
+#print("fsfsfs", end="")
+#print("1221212")
 
 
 
