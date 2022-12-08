38b2aa234fb7fda109f37ba575f59351e8872f78
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-12-08 13:18:48 +0100

    ss

--- véletlenszam.py
+++ véletlenszam.py
@@ -14,3 +14,20 @@ for i in range(10):
     l.append(random.randint(10,99))
 
 print(l)
+
+
+print(random.randint(-1000,1000)*3)
+l=[]
+for i in range(100):
+    l.append(random.randint(-1000,1000)*3)
+print(l)
+
+l5=[]
+print(sum(l))
+for e in l:
+    if e%5==0:
+        l5.append(e)
+print(l5)
+
+l5=[e//l5 for e in l if e%5==0]
+print(l5)
