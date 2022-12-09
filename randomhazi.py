73d088e1331c65955b19a54df19ee3f4dc760fe9
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-12-09 08:44:17 +0100

    ss

--- randomhazi.py
+++ randomhazi.py
@@ -1,11 +1,17 @@
 import random
 
-y=0
-while y==0:
-    szam=random.randint(1000,9999)
-    if szam %6==0: 
-        y=1
-        print(szam)
-    elif szam %12!=0:
-        y=1
-        print(szam)
+#y=0
+#while y==0:
+#    szam=random.randint(1000,9999)
+#    if szam %6==0: 
+#        y=1
+#        print(szam)
+#    elif szam %12!=0:
+#        y=1
+#print(szam)
+
+#más megoldás
+#print(random.randrage(167,1667,2)*6)
+
+#más megoldás
+print((random.randint(83,832)*2+1)*6)
---
+++ randomszavak.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+szavak=["alma","körte","barack","banán","dinnye","szőlő"]
+
+#random.seed(1)
+print(szavak[random.randint(0,len(szavak)-1)])
+
+print(random.choice(szavak))
