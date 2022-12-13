8067a386419ebddcc29c250893d81f01e09f0183
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-12-13 14:46:58 +0100

    s

--- randomhazi.py
+++ randomhazi.py
@@ -15,3 +15,27 @@ import random
 
 #más megoldás
 print((random.randint(83,832)*2+1)*6)
+
+szavak=["alma","körte","barack","banán","dinnye","szőlő"]
+
+#random.seed(1)
+print(szavak[random.randint(0,len(szavak)-1)])
+
+print(random.choice(szavak))
+
+#[["alma",14]],["körte",18],[]...]
+print("-",20)
+
+nagyLista=[]
+for e in szavak:
+    print(e)
+    kisLista=[]
+    kisLista.append(e)
+    kisLista.append(random.randint(12,312))
+    print(kisLista)
+    nagyLista.append(kisLista)
+    
+print(nagyLista)
+
+for e in nagyLista:
+    print(e[0].ljust(10),str(e[1]).rjust(5),"kg")
