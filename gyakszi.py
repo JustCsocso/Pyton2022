3a23f760c5ee78d4917be11ba44b5fab7924c5e9
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2022-12-16 10:35:52 +0100

    s

--- gyakszi.py
+++ gyakszi.py
@@ -14,4 +14,21 @@ print(lista)
 legnagyobb=max(lista)
 egyseg=80/legnagyobb
 for e in lista:
-    print("**"(e*egyseg))
+    print("**"*math.floor(e*egyseg))
+
+#3 jegyű szám bekérése:
+szam="12"
+while len(szam)!=3:
+    szam=input("Kérek egy 3 jegyű számot: ")
+
+szam=int(szam)
+if szam%12==0:
+    print("Osztható")
+print(szam)
+
+
+szoveg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur nunc at aliquam varius. Praesent odio neque, malesuada nec dolor ac, sagittis pretium neque. Nullam mollis lorem id turpis ullamcorper fringilla non a augue. Maecenas blandit massa lectus, euismod interdum diam vehicula eget. Mauris lectus dui, vehicula at velit ac, fermentum blandit eros. Integer tristique sed erat at fringilla. Ut a dui ac nulla tristique maximus in in diam."
+betu=""
+print(len(szoveg.split(" ")))
+szoveg2=szoveg.replace(betu,betu.upper())
+print(szoveg2)
---
+++ hazi.py
@@ -0,0 +1,20 @@
+import random
+import math
+
+Matyus=[]
+for i in range(10):
+    Matyus.append(random.randint(1,23))
+
+Lacko=[]
+for i in range(10):
+    Lacko.append(random.randint(1,40))
+
+Oliver=[]
+for i in range(10):
+    Oliver.append(random.randint(2022,2023))
+print("Év="+str(Oliver))
+print()
+print("Hónap="+str(Lacko))
+print()
+print("Nap="+str(Matyus))
+print()
