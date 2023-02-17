bf0ef25552da1933ced42e1073b1b23e1f965e81
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-02-17 09:38:35 +0100

    hrt

--- érettségi feladatok/agazativizsga2020_21/haromszopg.py
+++ érettségi feladatok/agazativizsga2020_21/haromszopg.py
@@ -1,1 +1,13 @@
-s
+def haromszopg():
+
+   vissza=[]  
+   for i in range(3):
+      szam=""
+      while szam=="":
+         try:
+            szam=int(input("Kérek egy egész számot: "))
+         except:
+            print("Ez nem egész szám!")
+
+      vissza.append(szam)
+   return vissza
