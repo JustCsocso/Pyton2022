e165d4990b319f5cd74ff23e0b436d01946b3000
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-03-21 11:29:50 +0100

    s

---
+++ érettségi feladatok/kémia/kémiai.py
@@ -0,0 +1,39 @@
+
+f=open("felfedezesek.csv")
+elemek=[]
+for e in f:
+    elemek.append(e.replace("\n","").split(";"))
+
+
+f.close()
+
+#print(elemek[0])
+elemek.pop(0)
+#print(elemek[0])
+
+
+print("3. feladat: Az elemek száma: {}".format(len(elemek)))
+
+okor=[e for e in elemek if e[0]=="Ókor"]
+print("4. feladat: A felfedezések száma az ókorban: {}".format(len(okor)))
+
+#print("a"<"b")
+while True:
+    vegyjel=input("5. feladat: Kérek egy vegyjelet: ".lower())
+    if len(vegyjel)<3 and len(vegyjel)>0:
+        jo=True
+        for i in range(len(vegyjel)):
+            if not(vegyjel[i]>="a" and vegyjel[i]<="z"):
+                jo=False
+        if jo:
+            break
+                
+
+#print(vegyjel)
+
+kereset=[e for e in elemek if e[2].lower()==vegyjel]
+if kereset==[]:
+    print("Nincs ilyen")
+else:
+    p
+#print(kereset)
