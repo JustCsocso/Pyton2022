11150eb843b3de869bf7be3176efb0d9a245dbaf
Pintér Csongor <pinterc21@ADY-NAGYATAD.SULINET.HU>
2023-03-22 13:13:28 +0100

    deef

---
+++ add/project.py
@@ -0,0 +1,62 @@
+#nevek=[]
+#for e in nevek:
+
+ora=input("Kérek egy órát: ")
+if ora=="1":
+    print("Ötletelés elkezdése, illetve első fájl létrehozása, ami a szám kitalálós volt")
+
+if ora=="2":
+    nevek=input("Kérek egy nevet: ")
+    if nevek=="Laci":
+        print("Elkezdett egy új python programon gondolkodni, ami a kő-papír-olló volt")
+    elif nevek =="Csongi":
+        print("Hiányzás")
+    elif nevek =="Martin":
+        print("Számlálós játék folytatása")
+
+
+
+if ora=="3":
+    nevek=input("Kérek egy nevet: ")
+    if nevek=="Laci":
+        print("Elkezdte a kő-papír-olló programot ")
+    elif nevek =="Csongi":
+        print("Hiányzás")
+    elif nevek =="Martin":
+        print("Számlálós játék folytatása")
+
+if ora=="4":
+    nevek=input("Kérek egy nevet: ")
+    if nevek=="Laci":
+        print("a kő-papír-olló kibővítése kalapáccsal és")
+    elif nevek =="Csongi":
+        print("Hiányzás")
+    elif nevek =="Martin":
+        print("Számlálós játékhoz kitalálta, hogy ne csak a felhasználó, hanem a gép is találja ki a mi általunk kitalált programot")
+
+if ora=="5":
+    nevek=input("Kérek egy nevet: ")
+    if nevek=="Laci":
+        print("Kibővített programjának folytatása")
+    elif nevek =="Csongi":
+        print("Hiányzás")
+    elif nevek =="Martin":
+        print("Számlálós játék kibővitett változatának a folytatása")
+
+if ora=="6":
+    nevek=input("Kérek egy nevet: ")
+    if nevek=="Laci":
+        print("Kibővített programjának folytatása")
+    elif nevek =="Csongi":
+        print("Saját programjának kigondolása")
+    elif nevek =="Martin":
+        print("Számlálós játék kibővitett változatának a folytatása")
+
+if ora=="7":
+    nevek=input("Kérek egy nevet: ")
+    if nevek=="Laci":
+        print("Kibővített programjának folytatása")
+    elif nevek =="Csongi":
+        print("")
+    elif nevek =="Martin":
+        print("Számlálós játék kibővitett változatának a folytatása")
